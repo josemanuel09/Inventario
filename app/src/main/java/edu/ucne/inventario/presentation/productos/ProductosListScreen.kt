9fb831f64ec2fc59af4dee53b8b6a6b608af9b94
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-10-16 17:23:15 -0400

    Agregando ProductosListScreen.kt

---
+++ app/src/main/java/edu/ucne/inventario/presentation/productos/ProductosListScreen.kt
@@ -0,0 +1,246 @@
+package edu.ucne.inventario.presentation.productos
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import edu.ucne.inventario.data.remote.dto.ProductoDto
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+@Composable
+fun ProductoListScreen(
+    drawerState: DrawerState,
+    scope: CoroutineScope,
+    viewModel: ProductosViewModel = hiltViewModel(),
+    createProducto: () -> Unit,
+    onDelete: (Int) -> Unit,
+    onEdit: (Int) -> Unit
+){
+    val uiState = viewModel.uiState.collectAsStateWithLifecycle()
+
+    ProductoListBodyScreen(
+        drawerState = drawerState,
+        scope = scope,
+        uiState = uiState.value,
+        createProducto = createProducto,
+        onDelete = onDelete,
+        onEdit = onEdit
+
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProductoListBodyScreen(
+    drawerState: DrawerState,
+    scope: CoroutineScope,
+    uiState: ProductosViewModel.UiState,
+    createProducto: () -> Unit,
+    onDelete: (Int) -> Unit,
+    onEdit: (Int) -> Unit,
+){
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = "Lista de Productos",
+                        style = TextStyle(
+                            fontSize = 22.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Color.White
+                        )
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = {
+                        scope.launch { drawerState.open() }
+                    }) {
+                        Icon(imageVector = Icons.Default.Menu, contentDescription = "Ir al menú")
+                    }
+                },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = Color(0xFF6200EE)
+                )
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = createProducto,
+                modifier = Modifier.padding(16.dp),
+                containerColor = MaterialTheme.colorScheme.primary,
+                contentColor = Color.White
+            ) {
+                Icon(Icons.Filled.Add, contentDescription = "Añadir Producto")
+            }
+        }
+    ) { paddingValues ->
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(16.dp)
+        ) {
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                items(uiState.productos) { producto ->
+                    ProductoRow(
+                        producto = producto,
+                        onDelete = onDelete,
+                        onEdit = onEdit,
+                        categoriaNom = uiState.categorias.find { it.categoriaId == producto.categoriaId }?.nombre ?: "Desconocido",
+                        proveedorNom = uiState.proveedores.find { it.proovedorId == producto.proovedorId }?.nombre ?: "Desconocido"
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ProductoRow(
+    producto: ProductoDto,
+    categoriaNom: String,
+    proveedorNom: String,
+    onDelete: (Int) -> Unit,
+    onEdit: (Int) -> Unit,
+){
+    var expanded by remember { mutableStateOf(false) }
+    Card(
+        shape = MaterialTheme.shapes.medium,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp),
+        elevation = CardDefaults.cardElevation(6.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .background(MaterialTheme.colorScheme.surfaceVariant),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column(
+                modifier = Modifier.weight(5f),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = producto.nombre,
+                    style = TextStyle(
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.onSurface
+                    )
+                )
+                Text(
+                    text = "Descripcion: ${producto.descripcion}",
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.7f)
+                    )
+                )
+                Text(
+                    text = "Precio: ${producto.precio}",
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.7f)
+                    )
+                )
+                Text(
+                    text = "Cantidad: ${producto.cantidad}",
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.7f)
+                    )
+                )
+                Text(
+                    text = "Categoria: $categoriaNom",
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.7f)
+                    )
+                )
+                Text(
+                    text = "Proveedor: $proveedorNom",
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.7f)
+                    )
+                )
+            }
+            IconButton(
+                onClick = {expanded = !expanded},
+                modifier = Modifier.weight(1f)
+            ) {
+                Icon(Icons.Filled.MoreVert, contentDescription = "Más opciones")
+            }
+
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+                modifier = Modifier.background(MaterialTheme.colorScheme.surface)
+            ){
+
+                DropdownMenuItem(
+                    text = { Text("Eliminar") },
+                    leadingIcon = { Icon(Icons.Filled.Edit, contentDescription = "Eliminar")},
+                    onClick = {
+                        expanded = false
+                        onDelete(producto.productoId!!)
+                    }
+                )
+                DropdownMenuItem(
+                    text = { Text("Editar") },
+                    leadingIcon = { Icon(Icons.Filled.Edit, contentDescription = "Editar") },
+                    onClick = {
+                        expanded = false
+                        onEdit(producto.productoId!!)
+                    }
+                )
+            }
+        }
+    }
+}
