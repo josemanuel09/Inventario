1ab3f6323225c68be9bdbc0d0f02a37913174ebd
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-10-16 17:25:48 -0400

    Agregando Base de datos

---
+++ app/src/main/java/edu/ucne/inventario/data/local/database/InventarioDb.kt
@@ -0,0 +1,28 @@
+package edu.ucne.inventario.data.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import edu.ucne.inventario.data.local.dao.CategoriaDao
+import edu.ucne.inventario.data.local.dao.ProductoDao
+import edu.ucne.inventario.data.local.dao.ProovedorDao
+import edu.ucne.inventario.data.local.entities.CategoriaEntity
+import edu.ucne.inventario.data.local.entities.ProductoEntity
+import edu.ucne.inventario.data.local.entities.ProovedorEntity
+
+@Database(
+    entities = [
+        ProductoEntity::class,
+        CategoriaEntity::class,
+        ProovedorEntity::class
+    ],
+    version = 1,
+    exportSchema = false
+
+)
+
+abstract class InventarioDb: RoomDatabase() {
+    abstract val productoDao: ProductoDao
+    abstract val categoriaDao: CategoriaDao
+    abstract val proovedorDao: ProovedorDao
+
+}
