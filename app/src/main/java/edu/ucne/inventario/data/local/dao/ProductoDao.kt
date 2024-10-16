552331fffe6f99cd6055a36fc684ab7082dfe3e5
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-10-16 17:24:11 -0400

    Agregando ProductoDao

---
+++ app/src/main/java/edu/ucne/inventario/data/local/dao/ProductoDao.kt
@@ -0,0 +1,25 @@
+package edu.ucne.inventario.data.local.dao
+
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import edu.ucne.inventario.data.local.entities.ProductoEntity
+import kotlinx.coroutines.flow.Flow
+
+interface ProductoDao {
+    @Upsert
+    suspend fun save(productoEntity: ProductoEntity)
+    @Query("""
+        SELECT * FROM Productos
+        WHERE productoId = :productoId
+        LIMIT 1
+    """)
+    suspend fun getProducto(productoId: Int): ProductoEntity?
+
+    @Delete
+    suspend fun deleteProducto(productoEntity: ProductoEntity)
+    @Query("SELECT * FROM Productos")
+    fun getProductos(): Flow<List<ProductoEntity>>
+
+
+}
