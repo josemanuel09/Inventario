5a1d8ffd160e8b3a2a15b4af895fa434d5bc6718
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-10-16 17:30:02 -0400

    implementando AppModule

---
+++ app/src/main/java/edu/ucne/inventario/data/di/AppModule.kt
@@ -0,0 +1,61 @@
+package edu.ucne.inventario.data.di
+
+import android.content.Context
+import androidx.room.Room
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.inventario.data.di.AppModule.BASE_URL
+import edu.ucne.inventario.data.remote.interfaces.CategoriaApi
+import edu.ucne.inventario.data.remote.interfaces.ProductoApi
+import edu.ucne.inventario.data.remote.interfaces.ProovedorApi
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+    const val BASE_URL = "https://inventarioapi-f3c6fvgsh0f2aueq.eastus2-01.azurewebsites.net"
+
+    @Singleton
+    @Provides
+    fun providesMoshi(): Moshi =
+        Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+    @Provides
+    @Singleton
+    fun providesProductosApi(moshi: Moshi): ProductoApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(ProductoApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun providesCategoriaApi(moshi: Moshi): CategoriaApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(CategoriaApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun providesProovedorApi(moshi: Moshi): ProovedorApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(ProovedorApi::class.java)
+    }
+}
