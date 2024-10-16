9c2ac673c4dfd75020be415cd1fee94cfe654af2
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-10-16 17:25:09 -0400

    Modificando MainActivity

--- app/src/main/java/edu/ucne/inventario/MainActivity.kt
+++ app/src/main/java/edu/ucne/inventario/MainActivity.kt
@@ -6,12 +6,22 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.outlined.Build
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.inventario.presentation.navigation.InventarioNavHost
 import edu.ucne.inventario.ui.theme.InventarioTheme
+
+@AndroidEntryPoint
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +29,39 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             InventarioTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val navHost = rememberNavController()
+                val items = NavigationItems()
+                InventarioNavHost(navHostController = navHost, items = items)
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun NavigationItems() : List<NavigationItem> {
+    return listOf(
+        NavigationItem(
+            title = "Proovedores",
+            selectedIcon = Icons.Filled.Info,
+            unselectedIcon = Icons.Outlined.Info,
+
+            ),
+        NavigationItem(
+            title = "Categorias",
+            selectedIcon = Icons.Filled.Info,
+            unselectedIcon = Icons.Outlined.Build,
+        ),
+        NavigationItem(
+            title = "Productos",
+            selectedIcon = Icons.Filled.Info,
+            unselectedIcon = Icons.Outlined.Build,
+        )
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    InventarioTheme {
-        Greeting("Android")
-    }
+fun InventarioTheme(content: @Composable () -> Unit) {
+    MaterialTheme(
+        content = content
+    )
 }
