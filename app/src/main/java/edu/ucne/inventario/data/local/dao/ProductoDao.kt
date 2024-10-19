000f94f97602bfc4ec180ed9de44906b1828a465
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-10-19 16:24:57 -0400

    Agregando Anotacion Dao

--- app/src/main/java/edu/ucne/inventario/data/local/dao/ProductoDao.kt
+++ app/src/main/java/edu/ucne/inventario/data/local/dao/ProductoDao.kt
@@ -1,11 +1,13 @@
 package edu.ucne.inventario.data.local.dao
 
+import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Query
 import androidx.room.Upsert
 import edu.ucne.inventario.data.local.entities.ProductoEntity
 import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface ProductoDao {
     @Upsert
     suspend fun save(productoEntity: ProductoEntity)
