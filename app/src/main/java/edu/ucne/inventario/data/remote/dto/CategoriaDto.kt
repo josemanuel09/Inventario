946545aa822ac70a9f84d7f38cd1b0382329218e
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-10-19 16:20:26 -0400

    Modificando CategoriaDto

--- app/src/main/java/edu/ucne/inventario/data/remote/dto/CategoriaDto.kt
+++ app/src/main/java/edu/ucne/inventario/data/remote/dto/CategoriaDto.kt
@@ -1,8 +1,19 @@
 package edu.ucne.inventario.data.remote.dto
 
+import edu.ucne.inventario.data.local.entities.CategoriaEntity
+
 data class CategoriaDto(
     val categoriaId: Int?,
     val nombre: String,
     val descripcion: String
 
 )
+
+fun CategoriaDto.toEntity(): CategoriaEntity{
+    return CategoriaEntity(
+        categoriaId = categoriaId ?: 0,
+        nombre = nombre,
+        descripcion = descripcion
+    )
+
+}
