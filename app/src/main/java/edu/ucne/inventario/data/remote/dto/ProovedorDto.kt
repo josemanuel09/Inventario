f883f74c7c78f4cefb4a1441d248c7f2a2fc64ba
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-10-19 16:19:47 -0400

    Modificando ProovedorDto

--- app/src/main/java/edu/ucne/inventario/data/remote/dto/ProovedorDto.kt
+++ app/src/main/java/edu/ucne/inventario/data/remote/dto/ProovedorDto.kt
@@ -1,8 +1,19 @@
 package edu.ucne.inventario.data.remote.dto
 
+import edu.ucne.inventario.data.local.entities.ProovedorEntity
+
 data class ProovedorDto(
     val proovedorId: Int?,
     val nombre: String,
     val contacto: String,
     val direccion: String
 )
+
+fun ProovedorDto.toEntity(): ProovedorEntity {
+    return ProovedorEntity(
+        proovedorId = proovedorId ?: 0,
+        nombre = nombre,
+        contacto = contacto,
+        direccion = direccion
+    )
+}
