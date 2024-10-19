d0442ee00587a933cef312220fda2c3cf034b3fa
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-10-19 16:25:33 -0400

    Modificando ProductoDto

--- app/src/main/java/edu/ucne/inventario/data/remote/dto/ProductoDto.kt
+++ app/src/main/java/edu/ucne/inventario/data/remote/dto/ProductoDto.kt
@@ -1,5 +1,7 @@
 package edu.ucne.inventario.data.remote.dto
 
+import edu.ucne.inventario.data.local.entities.ProductoEntity
+
 data class ProductoDto (
     val productoId: Int?,
     val nombre: String,
@@ -12,3 +14,15 @@ data class ProductoDto (
 
 
 )
+fun ProductoDto.toEntity(): ProductoEntity {
+    return ProductoEntity(
+        productoId = productoId ?: 0,
+        nombre = nombre,
+        descripcion = descripcion,
+        precio = precio,
+        cantidad = cantidad,
+        categoriaId = categoriaId,
+        proovedorId = proovedorId
+    )
+
+}
