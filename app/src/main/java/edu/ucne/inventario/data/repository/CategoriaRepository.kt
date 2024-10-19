236d8f7bffa98aaa0120175894a4177335071339
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-10-19 16:20:45 -0400

    Agregando Flow a CategoriaRepository

--- app/src/main/java/edu/ucne/inventario/data/repository/CategoriaRepository.kt
+++ app/src/main/java/edu/ucne/inventario/data/repository/CategoriaRepository.kt
@@ -1,16 +1,88 @@
 package edu.ucne.inventario.data.repository
 
+import android.util.Log
+import edu.ucne.inventario.data.local.dao.CategoriaDao
+import edu.ucne.inventario.data.local.entities.CategoriaEntity
+import edu.ucne.inventario.data.remote.RemoteDataSource
+import edu.ucne.inventario.data.remote.Resources
 import edu.ucne.inventario.data.remote.dto.CategoriaDto
-import edu.ucne.inventario.data.remote.interfaces.CategoriaApi
+import edu.ucne.inventario.data.remote.dto.toEntity
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
+import retrofit2.HttpException
 import retrofit2.Response
 import javax.inject.Inject
 
 class CategoriaRepository @Inject constructor(
-    private val categoriaApi: CategoriaApi
+    private val data: RemoteDataSource,
+    private val categoriaDao: CategoriaDao
 ) {
-    suspend fun getCategorias() = categoriaApi.getCategorias()
-    suspend fun getCategoriaById(id: Int) = categoriaApi.getCategoriaById(id)
-    suspend fun saveCategoria(categoria: CategoriaDto) = categoriaApi.saveCategoria(categoria)
-    suspend fun deleteCategoria(id: Int): Response<Void?> = categoriaApi.deleteCategoria(id)
+    fun getCategorias(): Flow<Resources<List<CategoriaEntity>>> = flow {
+        try {
+            emit(Resources.Loading())
+            val categoriaApi = data.getCategorias()
+            val categoriaRoom = categoriaApi.map { it.toEntity() }
+            categoriaRoom.forEach { categoriaDao.save(it) }
+
+            emit(Resources.Success(categoriaRoom))
+        } catch (e: HttpException) {
+            emit(Resources.Error(e.message ?: "Error HTTP DE INTERNET"))
+        } catch (e: Exception) {
+            val categoriaRoom = categoriaDao.getCategorias().firstOrNull()
+            if (categoriaRoom.isNullOrEmpty()) {
+                emit(Resources.Error(e.message ?: "Error DESCONOCIDO"))
+            } else {
+                emit(Resources.Success(categoriaRoom))
+            }
+        }
+    }
+
+
+    fun getCategoriaById(id: Int): Flow<Resources<CategoriaEntity>> = flow {
+        try{
+            emit(Resources.Loading())
+            val categoria = categoriaDao.getCategoria(id)
+            emit(Resources.Success(categoria!!))
+        }catch (e: HttpException){
+            emit(Resources.Error("ERROR DE INTERNET ${e.message}"))
+        }catch (e: Exception){
+            Log.e("Categoria Repository", "getCategoria: ${e.message}")
+            emit(Resources.Error(e.message ?: "Error DESCONOCIDO"))
+        }
+    }
+    fun saveCategoria(categoria: CategoriaDto): Flow<Resources<CategoriaEntity>> = flow {
+        try{
+            emit(Resources.Loading())
+            val categoriaApi = data.saveCategoria(categoria)
+            val categoriaRoom = categoriaApi.toEntity()
+            categoriaDao.save(categoriaRoom)
+            emit(Resources.Success(categoriaRoom))
+        }catch (e: HttpException){
+            emit(Resources.Error("Error de conexión: ${e.message()}"))
+        }catch (e: Exception) {
+            emit(Resources.Error("Error: ${e.message}"))
+        }
+    }
+
+    suspend fun deleteCategoria(id: Int): Resources<Unit> {
+       return try{
+           data.deleteCategoria(id)
+           val categoriaRoom = categoriaDao.getCategoria(id)
+           if (categoriaRoom != null){
+               categoriaDao.deleteCategoria(categoriaRoom)
+           }
+           Resources.Success(Unit)
+        }catch (e: HttpException){
+           Resources.Error("Error de conexión: ${e.message()}")
+       }catch (e: Exception){
+           val categoriaRoom = categoriaDao.getCategoria(id)
+           if (categoriaRoom != null){
+               categoriaDao.deleteCategoria(categoriaRoom)
+           }
+           Resources.Success(Unit)
+       }
+    }
 
 }
