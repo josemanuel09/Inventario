fbe3dce39e744071b859d83b793e424e55b2a980
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-10-19 16:16:33 -0400

    Agregando Flow al ProovedorRepository

--- app/src/main/java/edu/ucne/inventario/data/repository/ProovedorRepository.kt
+++ app/src/main/java/edu/ucne/inventario/data/repository/ProovedorRepository.kt
@@ -1,15 +1,86 @@
 package edu.ucne.inventario.data.repository
 
+import android.util.Log
+import edu.ucne.inventario.data.local.dao.ProovedorDao
+import edu.ucne.inventario.data.local.entities.ProovedorEntity
+import edu.ucne.inventario.data.remote.RemoteDataSource
+import edu.ucne.inventario.data.remote.Resources
 import edu.ucne.inventario.data.remote.dto.ProovedorDto
-import edu.ucne.inventario.data.remote.interfaces.ProovedorApi
-import retrofit2.Response
+import edu.ucne.inventario.data.remote.dto.toEntity
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
 import javax.inject.Inject
 
 class ProovedorRepository @Inject constructor(
-    private val proovedorApi: ProovedorApi
+    private val data: RemoteDataSource,
+    private val proovedorDao: ProovedorDao
 ) {
-    suspend fun getProovedores() = proovedorApi.getProovedores()
-    suspend fun getProovedorById(id: Int) = proovedorApi.getProovedorById(id)
-    suspend fun saveProovedor(proovedor: ProovedorDto) = proovedorApi.saveProovedor(proovedor)
-    suspend fun deleteProovedor(id: Int): Response<Void?> = proovedorApi.deleteProovedor(id)
+
+    fun getProovedores(): Flow<Resources<List<ProovedorEntity>>> = flow {
+        try {
+            emit(Resources.Loading())
+            val proovedorApi = data.getProovedores()
+            val proovedorRoom = proovedorApi.map { it.toEntity() }
+            proovedorRoom.forEach { proovedorDao.save(it) }
+
+            emit(Resources.Success(proovedorRoom))
+        } catch (e: HttpException) {
+            emit(Resources.Error(e.message ?: "Error HTTP DE INTERNET"))
+        } catch (e: Exception) {
+            val proovedorRoom = proovedorDao.getProovedores().firstOrNull()
+            if (proovedorRoom.isNullOrEmpty()) {
+                emit(Resources.Error(e.message ?: "Error DESCONOCIDO"))
+            } else {
+                emit(Resources.Success(proovedorRoom))
+            }
+        }
+    }
+
+    fun getProovedorById(id: Int): Flow<Resources<ProovedorEntity>> = flow {
+        try {
+            emit(Resources.Loading())
+            val proovedor = proovedorDao.getProovedor(id)
+            emit(Resources.Success(proovedor!!))
+        } catch (e: HttpException) {
+            emit(Resources.Error("ERROR DE INTERNET ${e.message()}"))
+        } catch (e: Exception) {
+            Log.e("Proovedor Repository", "getProovedor: ${e.message}")
+            emit(Resources.Error(e.message ?: "Error DESCONOCIDO"))
+        }
+    }
+
+    fun saveProovedor(proovedor: ProovedorDto): Flow<Resources<ProovedorEntity>> = flow {
+        try {
+            emit(Resources.Loading())
+            val proovedorApi = data.saveProovedor(proovedor)
+            val proovedorRoom = proovedorApi.toEntity()
+            proovedorDao.save(proovedorRoom)
+            emit(Resources.Success(proovedorRoom))
+        } catch (e: HttpException) {
+            emit(Resources.Error("Error de conexión: ${e.message()}"))
+        } catch (e: Exception) {
+            emit(Resources.Error("Error: ${e.message}"))
+        }
+    }
+
+    suspend fun deleteProovedor(id: Int): Resources<Unit> {
+        return try {
+            data.deleteProovedor(id)
+            val proovedorRoom = proovedorDao.getProovedor(id)
+            if (proovedorRoom != null) {
+                proovedorDao.deleteProovedor(proovedorRoom)
+            }
+            Resources.Success(Unit)
+        } catch (e: HttpException) {
+            Resources.Error("Error de conexión: ${e.message()}")
+        } catch (e: Exception) {
+            val proovedorRoom = proovedorDao.getProovedor(id)
+            if (proovedorRoom != null) {
+                proovedorDao.deleteProovedor(proovedorRoom)
+            }
+            Resources.Success(Unit)
+        }
+    }
 }
